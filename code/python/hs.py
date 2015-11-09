d4227a45df56890c9493aa8a2db75320c3e78a8c
Siqi Xie <sx8tq@virginia.edu>
2015-11-09 11:35:55 -0500

    upload hs file

--- code/python/hs.py
+++ code/python/hs.py
@@ -3,9 +3,10 @@ def hs(n):
     while (n != 1):
         print(n)
         count = count + 1
-        if ( n%2 == 0 ):
+        if (n % 2 == 0):
             n = n//2
         else:
             n = 3 * n + 1
-    print(n)
+    print(1)
     print(count)
+    return None
